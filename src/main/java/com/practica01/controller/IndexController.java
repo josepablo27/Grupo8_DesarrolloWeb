c9d84d093e8526d4bd6ffd49475777142b22a08d
josepablo27 <jduran60412@ufide.ac.cr>
2022-06-23 15:30:49 -0600

    commit

--- src/main/java/com/practica01/controller/IndexController.java
+++ src/main/java/com/practica01/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.practica01.controller;
 
+
+import com.practica01.domain.Estado;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.practica01.service.EstadoService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -26,4 +29,27 @@ public class IndexController {
         return "index";
     
 }
+     @GetMapping("/nuevoEstado")
+    public String nuevoEstado(Estado estado){
+        return "modificarEstado";
+    }
+    
+    @PostMapping("/guardarEstado")
+    public String guardarEstado(Estado estado){
+        estadoService.save(estado);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarEstado/{id_estado}")
+    public String modificarEstado(Estado estado, Model model){
+        estado = estadoService.getEstado(estado);
+        model.addAttribute("estado", estado);
+        return "modificarEstado";
+    }
+    
+    @GetMapping("/eliminarEstado/{id_estado}")
+    public String eliminarEstado(Estado estado){
+        estadoService.delete(estado);
+        return "redirect:/";
+    }
 }
